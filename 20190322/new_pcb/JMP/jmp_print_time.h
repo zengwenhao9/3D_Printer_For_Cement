56e6681a2ff54e2bff788549e2e4a1e4612aa014
zengwenhao9 <644356425@qq.com>
2019-03-22 17:53:23 +0800

    20190322

---
+++ 20190322/new_pcb/JMP/jmp_print_time.h
@@ -0,0 +1,16 @@
+#ifndef _JMP_PRINT_TIME_H
+#define _JMP_PRINT_TIME_H
+#include "tmsys.h"
+
+
+void jmp_print_time_init(void);
+
+
+
+extern u32 jmp_print_run_time;
+extern u32 jmp_print_remain_time;
+
+
+#endif
+
+
---
+++ 20190322/new_pcb/USER/main.c
@@ -0,0 +1,91 @@
+#include "tmsys.h"
+#include "tmdelay.h" 
+#include "bsp_uart.h"
+#include "bsp_gpio.h"
+#include "bsp_sdram.h"
+#include "bsp_test.h"
+#include "usb_test.h"
+#include "usb_app.h"
+
+#include "app_qspi_flash_fatfs.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "croutine.h"
+
+#include "jmp_config.h"
+#include "jmp_storage_read.h"
+#include "jmp_gcode_analysis.h"
+#include "jmp_command_execution.h"
+#include "jmp_param_state.h"
+#include "jmp_gui.h"
+#include "jmp_motion.h"
+#include "jmp_print_time.h"
+
+static TaskHandle_t xHandleTaskLED = NULL;
+
+HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority)
+{
+  return HAL_OK;
+}
+
+void HAL_Delay(uint32_t Delay)
+{
+	tmdelay_ms(Delay);
+}
+
+static void vTaskLED(void *pvParameters)
+{
+	while(1)
+	{
+		bsp_led_run_control(1);
+		vTaskDelay(1000);
+		bsp_led_run_control(0);
+		vTaskDelay(1000);
+	}
+}
+
+int main(void)
+{
+	__set_PRIMASK(1);
+	Cache_Enable();                             //打开L1-Cache
+	HAL_Init();				                    //初始化HAL库
+	Stm32_Clock_Init(160,5,2,4);                //设置时钟,400Mhz 
+	tmdelay_init();
+	
+	bsp_uart1_init(115200);
+	bsp_uart2_init(115200);
+	bsp_uart3_init(115200);
+	bsp_uart4_init(115200);
+
+	bsp_gpio_init();
+	bsp_sdram_init();
+	
+	usb_app_init();
+	
+	app_qspi_flash_fatfs_init();
+	
+	jmp_ex_config_init();
+	jmp_configstate_init();
+	jmp_storage_read_init();
+	jmp_command_execution_init();
+	jmp_gui_init();
+	jmp_motion_init();
+	jmp_print_time_init();
+	
+	
+	xTaskCreate( vTaskLED,
+							"vTaskLED",
+								512,
+								NULL,
+								2,
+								&xHandleTaskLED );
+								
+	
+	vTaskStartScheduler();
+	
+	while(1)
+	{
+	}
+}
